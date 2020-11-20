110441e3e5489ef636c395c6f2f5ee332a705449
sanya4 <sharma.sanya411@gmail.com>
2020-11-20 13:20:58 +0530

    my maven 1st commit

---
+++ MavenExample/src/main/java/com/lti/mavendemo/Welcome.java
@@ -0,0 +1,11 @@
+package com.lti.mavendemo;
+
+public class Welcome
+{
+	
+public String show()
+{
+	return "hello devops";
+}
+
+}
---
+++ MavenExample/src/test/java/com/lti/mavendemo/TestWelcome.java
@@ -0,0 +1,15 @@
+package com.lti.mavendemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestWelcome {
+
+	@Test
+   public void testShow() {        
+        Welcome w= new Welcome();        
+        assertEquals("hello devops",w.show());
+    }
+
+}
